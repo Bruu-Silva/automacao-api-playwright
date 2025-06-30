d483f3815d975d0c2dae2fe444e69fe7ffa6794f
Bruna-SSilva <bruna.silva@atlastechnol.com>
2025-06-29 21:44:36 -0300

    feat: created API tests for get/users method

--- api/UsersApi.ts
+++ api/UsersApi.ts
@@ -3,6 +3,10 @@ import { APIRequestContext, APIResponse } from '@playwright/test'
 export class UsersAPI {
   constructor(private request: APIRequestContext) {}
 
+  async createUser(payload: object): Promise<APIResponse> {
+    return await this.request.post('/api/users', { data: payload })
+  }
+
   async getUserById(userId: number): Promise<APIResponse> {
     return await this.request.get(`/api/users/${userId}`)
   }
